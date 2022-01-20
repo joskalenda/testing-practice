76d6b7edfec49c9e6a8b9aba011438549c7fb3bb
Topaz Jos <topazjos@Topaz.local>
2022-01-20 17:24:53 +0200

    Finish the reverse string function

---
+++ reverseString/reverse.js
@@ -0,0 +1,9 @@
+const toReverse = (s) => {
+  let str = "";
+  for (let i = s.length -1; i >= 0; i -= 1){
+    str += s[i];
+  }
+  return str;
+};
+
+module.exports = toReverse;
---
+++ reverseString/reverse.test.js
@@ -0,0 +1,4 @@
+const toReverse = require('./reverse');
+it('String is reversed', () => {
+  expect(toReverse('kalenda')).toBe('adnelak')
+});
